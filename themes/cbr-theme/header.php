13faababd859216e41feb307c15736cac760c177
Rishik Zaparde <rz293@cornell.edu>
2020-10-20 14:48:04 -0400

    slightly working navbar with skeleton code

---
+++ themes/cbr-theme/header.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title><?php echo get_bloginfo( 'name' ); ?></title>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?php echo get_bloginfo( 'template_directory' );?>/blog.css" rel="stylesheet">
+  <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <?php wp_head();?>
+</head>
+
+<body>
+
+<div class="blog-masthead">
+	<div class="nav-container">
+		<nav class="blog-nav">
+      <img src="wp-content/uploads/2020/10/cbr-logo.png" alt="CBR logo"> 
+      <div class="other-links">
+        <?php wp_list_pages( '&title_li=' ); ?>
+      </div>
+		</nav>
+	</div>
+</div>
+
+
