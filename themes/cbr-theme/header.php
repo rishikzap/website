abf6224d8deaee22a92b37b597eaa589a434e0e1
Rishik Zaparde <rz293@cornell.edu>
2020-10-22 15:23:10 -0400

    fixed navbar

--- themes/cbr-theme/header.php
+++ themes/cbr-theme/header.php
@@ -9,14 +9,18 @@
   <meta name="author" content="">
   <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link href="<?php echo get_bloginfo('template_directory'); ?>/after.css" rel="stylesheet">
 
-<nav class="navbar navbar-expand-md navbar-light bg-light" role="navigation">
+
+<nav class="navbar navbar-expand-md navbar-light" role="navigation">
   <div class="container">
     <!-- Brand and toggle get grouped for better mobile display -->
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-controls="bs-example-navbar-collapse-1" aria-expanded="false" aria-label="<?php esc_attr_e( 'Toggle navigation', 'your-theme-slug' ); ?>">
         <span class="navbar-toggler-icon"></span>
     </button>
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="http://localhost:8888/">
+      <img src="/wp-content/uploads/2020/10/cbr-logo.png" width="100" height="100" alt="">
+    </a>
         <?php
         wp_nav_menu( array(
             'theme_location'    => 'primary',
