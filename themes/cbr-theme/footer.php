b64563cc8e034fc9ddd459e87fc440b17da4916f
Rishik Zaparde <rz293@cornell.edu>
2020-10-22 23:16:23 -0400

    font size navbar

--- themes/cbr-theme/footer.php
+++ themes/cbr-theme/footer.php
@@ -1,10 +1,7 @@
 </div> <!-- /.container -->
 
     <footer class="blog-footer">
-      <p>Blog template built for <a href="http://getbootstrap.com">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>
-      <p>
         <a href="#">Back to top</a>
-      </p>
     </footer>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
