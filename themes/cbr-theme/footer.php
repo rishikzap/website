b9b9a447cd854fea4fb5cfa8f154cbfb13fd1d80
Rishik Zaparde <rz293@cornell.edu>
2020-10-31 16:14:35 -0400

    footer skeleton

--- themes/cbr-theme/footer.php
+++ themes/cbr-theme/footer.php
@@ -1,8 +1,101 @@
 </div> <!-- /.container -->
 
-    <footer class="blog-footer">
-        <a href="#">Back to top</a>
-    </footer>
+<!-- Footer -->
+<footer class="page-footer font-small indigo">
+
+  <!-- Footer Links -->
+  <div class="container text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 mx-auto border">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold mt-3 mb-4">Read More</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">CBR Now</a>
+          </li>
+          <li>
+            <a href="#!">Articles</a>
+          </li>
+          <li>
+            <a href="#!">Past Issues</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 mx-auto border">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold mt-3 mb-4">About Us</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Our Team</a>
+          </li>
+          <li>
+            <a href="#!">Advertise With Us</a>
+          </li>
+          <li>
+            <a href="#!">Other Inquiries</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 mx-auto border">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold mt-3 mb-4 ">Connect With Us!</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <a href="#!">Facebook</a>
+          </li>
+          <li>
+            <a href="#!">Instagram</a>
+          </li>
+          <li>
+            <a href="#!">Contact</a>
+          </li>
+          <li>
+            <a href="#!">Help</a>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">© 2020 Cornell Business Review 
+  </div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
