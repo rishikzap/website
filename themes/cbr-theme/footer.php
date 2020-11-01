02593d02f7576aab3567a43e130fe9d0915e7292
Rishik Zaparde <rz293@cornell.edu>
2020-11-01 13:11:16 -0500

    fixed dropdown issue

--- themes/cbr-theme/footer.php
+++ themes/cbr-theme/footer.php
@@ -95,8 +95,6 @@
 
 </footer>
 <!-- Footer -->
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <?php wp_footer(); ?>
   </body>
 </html>
--- themes/cbr-theme/header.php
+++ themes/cbr-theme/header.php
@@ -10,7 +10,8 @@
   <link href="<?php echo get_bloginfo('template_directory'); ?>/blog.css" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
   <link href="<?php echo get_bloginfo('template_directory'); ?>/after.css" rel="stylesheet">
-
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 
 <nav class="navbar navbar-expand-md navbar-light" role="navigation">
   <div class="container">
