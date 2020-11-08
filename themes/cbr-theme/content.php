e8790ce86d51c0e32ade1cc915defb7a773c0384
Helen <HelenWang170@gmail.com>
2020-11-08 15:58:57 -0500

    slight edit to content.php

--- themes/cbr-theme/content.php
+++ themes/cbr-theme/content.php
@@ -2,6 +2,6 @@
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
 
-  <?php the_excerpt(); ?>
+  <?php the_content(); ?>
 </div>
 <!-- /.blog-post -->
