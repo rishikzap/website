9db5ba7594d7b68502dc7b65c249b7aec2b1fc60
Rishik Zaparde <rz293@cornell.edu>
2020-11-15 13:01:16 -0500

    author page template added with links

--- themes/cbr-theme/content-single.php
+++ themes/cbr-theme/content-single.php
@@ -2,7 +2,7 @@
 <div class="blog-post">
 	<img id="feature-image" src="<?php $img=wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); echo $img[0]; ?>" alt="<?php the_title(); ?>"/>
 	<h2 class="blog-post-title"><?php the_title(); ?></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<p class="blog-post-meta"><?php the_date(); ?> by <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>"><?php the_author(); ?></a></p>
 	<?php the_content(); ?>
 	<div class="tags">
 		<?php
--- themes/cbr-theme/content.php
+++ themes/cbr-theme/content.php
@@ -1,6 +1,6 @@
 <div class="blog-post">
 	<h2 class="blog-post-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-	<p class="blog-post-meta"><?php the_date(); ?> by <a href="#"><?php the_author(); ?></a></p>
+	<p class="blog-post-meta"><?php the_date(); ?> by <a href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" title="<?php echo esc_attr( get_the_author() ); ?>"><?php the_author(); ?></a></p>
 
   <?php the_excerpt(); ?>
 </div>
