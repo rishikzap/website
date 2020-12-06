49751b8923159691d9ae007f38201fe09610db57
Helen <HelenWang170@gmail.com>
2020-12-06 16:33:52 -0500

    small changes to menu and author pages

--- themes/cbr-theme/author.php
+++ themes/cbr-theme/author.php
@@ -5,41 +5,42 @@
 // Set the Current Author Variable $curauth
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 ?>
+<div class="author-page">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div id="author-img">
+					<?php echo get_avatar( $curauth->user_email , '375 ', '', '', array('class' => array('img-responsive') )); ?>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<h2 id="author-name"><?php echo $curauth->display_name; ?></h2>
+				<p id="author-descrip"><?php echo $curauth->user_description; ?></p>
+			</div>
+		</div>
+	</div>
 
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <div id="author-img">
-                <?php echo get_avatar( $curauth->user_email , '375 ', '', '', array('class' => array('img-responsive') )); ?>
-            </div>
-        </div>
-        <div class="col-md-6">
-            <h2 id="author-name"><?php echo $curauth->display_name; ?></h2>
-            <p id="author-descrip"><?php echo $curauth->user_description; ?></p>
-        </div>
-    </div>
+			<h2 id="featured">Featured</h2>
+					<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+					<div class="mini">
+						<?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?>
+						<a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
+						<h3 class="title"><?php the_title(); ?>
+						</h3> </a>
+						<div class="excerpt"><?php the_excerpt(); ?></div>
+					</div>
+			<?php endwhile; 
+
+
+			// Previous/next page navigation.
+			the_posts_pagination();
+
+
+			else: ?>
+			<p><?php _e('No posts by this author.'); ?></p>
+
+			<?php endif; ?>
+
+
+	<?php get_footer(); ?>
 </div>
-
-        <h2 id="featured">Featured</h2>
-                <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-                <div class="mini">
-                    <?php echo get_the_post_thumbnail( $page->ID, 'thumbnail' ); ?>
-                    <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
-                    <h3 class="title"><?php the_title(); ?></a>
-                    </h3> 
-                    <div class="excerpt"><?php the_excerpt(); ?></div>
-                </div>
-        <?php endwhile; 
-
-        
-        // Previous/next page navigation.
-        the_posts_pagination();
-        
-        
-        else: ?>
-        <p><?php _e('No posts by this author.'); ?></p>
-        
-        <?php endif; ?>
-
-
-<?php get_footer(); ?>
