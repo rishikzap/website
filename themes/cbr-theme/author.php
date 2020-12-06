2e911e4f3e6530e2f8a3a98f374a8beffcf94d4e
Rishik Zaparde <rz293@cornell.edu>
2020-12-06 13:21:46 -0500

    author page styling

--- themes/cbr-theme/author.php
+++ themes/cbr-theme/author.php
@@ -5,25 +5,29 @@
 // Set the Current Author Variable $curauth
 $curauth = (isset($_GET['author_name'])) ? get_user_by('slug', $author_name) : get_userdata(intval($author));
 ?>
-     
-<div class="author-profile-card">
-    <h2><?php echo $curauth->display_name; ?></h2>
-    <div class="author-photo">
-    <?php echo get_avatar( $curauth->user_email , '90 '); ?>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-6">
+            <div id="author-img">
+                <?php echo get_avatar( $curauth->user_email , '375 ', '', '', array('class' => array('img-responsive') )); ?>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <h2 id="author-name"><?php echo $curauth->display_name; ?></h2>
+            <p id="author-descrip"><?php echo $curauth->user_description; ?></p>
+        </div>
     </div>
-    <p><?php echo $curauth->user_description; ?></p>
 </div>
      
-<h2>Featured</h2>
+<h2 id="featured">Featured</h2>
  
  
          <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 <h3>
 <a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link: <?php the_title(); ?>">
 <?php the_title(); ?></a>
-</h3>
-<p class="posted-on">Posted on: <?php the_time('M d, Y'); ?></p>
- 
+</h3> 
 <?php the_excerpt(); ?>
  
 <?php endwhile; 
